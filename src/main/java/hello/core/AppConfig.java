f4bfa847cfeaaba9bef92d77478d66b7ae28b392
박지훈 <jihun.park8@cj.net>
2022-03-01 20:54:15 +0900

    220301

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -19,13 +21,21 @@ import hello.core.order.OrderServiceImpl;
  * 2022/03/01        Jihun Park       최초 생성
  */
 public class AppConfig {
-    /* 생성자 주입을 통해 DI를 해준다! DIP를 지  */
-    public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+    /* 생성자 주입을 통해 DI를 해준다! DIP! */
+    public MemberService memberService(){ //어떤 MemberRepository 쓸지는 여기서 결정
+        return new MemberServiceImpl(memberRepository());
+    }
+    //명확하게 어떤 레파지토리를 쓰는지 명시!
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
