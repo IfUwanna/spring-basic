8add6788ba7842b06950678e366c216a1126f994
박지훈 <jihun.park8@cj.net>
2022-03-02 17:33:45 +0900

    220302

---
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -0,0 +1,27 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+
+/**
+ * packageName    : hello.core.web
+ * fileName       : LogDemoService
+ * author         : Jihun Park
+ * date           : 2022/03/02
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/03/02        Jihun Park       최초 생성
+ */
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    public void logic(String id) {
+        myLoggerProvider.getObject().log("service id = " + id);
+    }
+}
