13d897399e5536d451c9bb6e3974bb5b9bb838f4
박지훈 <jihun.park8@cj.net>
2022-03-02 16:25:47 +0900

    220302

---
+++ src/test/java/hello/core/scope/SingletonWIthPrototypeTest1.java
@@ -0,0 +1,122 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Provider;
+
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * packageName    : hello.core.scope
+ * fileName       : SingletonWIthPrototype
+ * author         : Jihun Park
+ * date           : 2022/03/02
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/03/02        Jihun Park       최초 생성
+ */
+public class SingletonWIthPrototypeTest1 {
+
+    @Test
+    void prototypeFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.addCount();
+        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean2.addCount();
+        assertThat(prototypeBean2.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(2); //싱글톤(클라)빈 생성된 시점에 주입된 프로토타입 빈을 유지함!
+    }
+
+
+    static class ClientBean {  // 2. ObjectProvider  , 3. Provider
+
+        @Autowired  //
+        //private ObjectProvider<PrototypeBean> prototypeBeanProvider; // 2.ObjectProvider
+        private Provider<PrototypeBean> prototypeBeanProvider; // Provider
+
+        public int logic() {
+            //PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+    }
+
+    /*
+    static class ClientBean {
+
+       private final PrototypeBean prototypeBean;// 생성시점에 주입이 되어있음!
+
+        @Autowired
+        public ClientBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+        public int logic() { prototypeBean.addCount();
+            int count = prototypeBean.getCount(); return count;
+        }
+
+    }*/
+/*
+    static class ClientBean2 {
+        @Autowired
+        private ApplicationContext ac;   // ac를 받는방
+
+        public int logic() {
+            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+    }*/
+
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init " + this);
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
